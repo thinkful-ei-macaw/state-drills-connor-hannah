d608121e08e29ff069fa7ef7cf3d370abf57ede2
Connor McCormack <mccormack.connor.m@gmail.com>
2020-03-05 00:46:48 -0600

    some commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
 import HelloWorld from './src/HelloWorld';
+import Tabs from './state/Tabs'
 
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
 
-function App() {
-  return (
-    <div className="App" >
-      
+class App extends React.Component {
+  render() {
+    return (
+    <div>
+      <Tabs tabs={tabsProp} />
     </div>
   );
+  }
 }
+
+
 
 export default App;
